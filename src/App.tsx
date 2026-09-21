d64850275b5a498e1e85474b46155eb6e8a1c0ef
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-21 15:29:26 +0900

    Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">ホーム</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
