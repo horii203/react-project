02dbf7989453190cfd548b81185404d69e7db3c9
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-21 13:29:59 +0900

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import MyButton from "./components/MyButton";
+import { useEffect, useState } from "react";
 
 function App() {
-  const handleClick = () => {
-    alert("クリックされました");
-  };
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("画面が表示されました");
+  }, []);
   return (
     <div>
-      <h1>ホーム</h1>
-      <MyButton text="クリック" onClick={handleClick} />
+      <h1>カウンター</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   );
 }
