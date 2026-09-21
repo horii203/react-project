bde02f841f1fd31dcdb1d36203180b069795d531
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-21 12:01:42 +0900

    条件分岐

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("田中");
+  const [isLogon, setIsLogin] = useState(false);
   return (
     <div>
-      <h1>こんにちは、{name}さん</h1>
-      <button onClick={() => setName("佐藤")}>名前を変更</button>
+      <h1>{isLogon ? "ログインしています" : "ログインしていません"}</h1>
+      <button onClick={() => setIsLogin(!isLogon)}>ログイン状態変更</button>
     </div>
   );
 }
