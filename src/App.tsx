4744b5a54e563ab94d8b52004c0729ad53a84d6d
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-21 13:38:44 +0900

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,29 @@ type User = {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => {
         setUsers(data);
+        setLoading(false);
       });
   }, []);
   return (
     <div>
       <h1>ユーザー一覧</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name}：{user.email}
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <p>読み込み中...</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name}：{user.email}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
