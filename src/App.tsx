040fe8ca73cc49dd94e5452b9eb0c010de561ef1
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-21 13:36:26 +0900

    APIからデータを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useEffect, useState } from "react";
 
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    console.log("画面が表示されました");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
   }, []);
   return (
     <div>
-      <h1>カウンター</h1>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <h1>ユーザー一覧</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}：{user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
